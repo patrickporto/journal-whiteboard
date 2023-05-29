d1ca9d86d5b11456dc7ed307866e127aaf76c8e4
Patrick Porto <patrick.s.porto@gmail.com>
2023-05-29 17:41:48 -0600

    chore: rename module to journal whiteboard

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const CANNONICAL_NAME = 'fvtt-module-template';
+export const CANNONICAL_NAME = 'journal-whiteboard';
 export const MODULE_NAME = 'FVTT Module Template';
 export const MODULE_PATH = `modules/${CANNONICAL_NAME}`;
 export const TEMPLATE_PATH = `${MODULE_PATH}/templates`;
