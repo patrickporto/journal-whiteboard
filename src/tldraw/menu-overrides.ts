239d950dea8c588d5a25ecdf272db986a67d6c1d
Patrick Porto <patrick.s.porto@gmail.com>
2023-06-03 11:37:35 -0600

    feat: add foundry document context menu

--- src/tldraw/menu-overrides.ts
+++ src/tldraw/menu-overrides.ts
@@ -1,61 +1,16 @@
 import { App } from "@tldraw/tldraw";
-import { MenuSchema } from "@tldraw/ui";
+import { MenuSchema, TldrawUiOverrides, ToolbarSchemaContextType } from "@tldraw/ui";
+import { debugService } from "../debug/debug.module";
 
-export const menuOverrides = {
-    actionsMenu(app: App, schema: MenuSchema) {
+export const menuOverrides: Partial<TldrawUiOverrides> = {
+    contextMenu: (app: App, schema: MenuSchema, helpers) => {
+        debugService.log('contextMenu', app, schema, helpers);
+        unregisterConversionsFromContextMenu(schema);
+        registerWhiteboardContextMenu(app, schema, helpers);
+        registerFoundryDocumentContextMenu(app, schema, helpers);
         return schema;
     },
-    actions(app: App, schema: MenuSchema) {
-        return schema;
-    },
-    contextMenu: (app, schema, helpers) => {
-        schema.splice(
-            schema.findIndex(item => item.id === 'conversions'),
-            1,
-        );
-        if (!helpers.oneSelected) {
-            schema.push({
-                id: 'preferences-group',
-                type: 'group',
-                checkbox: false,
-                disabled: false,
-                readonlyOk: true,
-                children: [
-                    {
-                        id: 'toggle-grid',
-                        type: 'item',
-                        actionItem: {
-                            id: 'action.toggle-grid',
-                            label: 'action.toggle-grid.menu',
-                            kbd: "$'",
-                            readonlyOk: true,
-                            onSelect: () => {
-                                app.setGridMode(!app.isGridMode);
-                            },
-                        },
-                        checked: true,
-                        readonlyOk: true,
-                    },
-                ],
-            });
-        }
-        return schema;
-    },
-    menu(app: App, schema: MenuSchema, helpers: any) {
-        for (const item of schema) {
-            if (item.id === 'menu' && item.type === 'group') {
-                item.children = item.children.filter(menuItem => {
-                    if (menuItem.id === 'file' && menuItem.type === 'submenu') {
-                        return false;
-                    }
-                    return true;
-                });
-            }
-        }
-
-        return schema;
-    },
-    toolbar(app: App, schema: MenuSchema, helpers: any) {
+    toolbar(app: App, schema: ToolbarSchemaContextType, helpers: any) {
         schema.splice(
             schema.findIndex(item => item.id === 'embed'),
             1,
@@ -63,3 +18,74 @@ export const menuOverrides = {
         return schema;
     },
 };
+
+function unregisterConversionsFromContextMenu(schema: MenuSchema) {
+    schema.splice(
+        schema.findIndex(item => item.id === 'conversions'),
+        1
+    );
+}
+
+function registerWhiteboardContextMenu(app: App, schema: MenuSchema, helpers: any) {
+    if (!helpers.oneSelected) {
+        schema.push({
+            id: 'preferences-group',
+            type: 'group',
+            checkbox: false,
+            disabled: false,
+            readonlyOk: true,
+            children: [
+                {
+                    id: 'toggle-grid',
+                    type: 'item',
+                    actionItem: {
+                        id: 'action.toggle-grid',
+                        label: 'action.toggle-grid.menu',
+                        kbd: "$'",
+                        readonlyOk: true,
+                        onSelect: () => {
+                            app.setGridMode(!app.isGridMode);
+                        },
+                    },
+                    checked: true,
+                    readonlyOk: true,
+                    disabled: false
+                },
+            ],
+        });
+    }
+}
+
+function registerFoundryDocumentContextMenu(app: App, schema: MenuSchema, helpers: any) {
+    const selectedShapes = app.selectedShapes
+    const selectedShape = selectedShapes?.[0]
+    debugService.log('registerFoundryDocumentContextMenu', selectedShapes, selectedShape);
+    if (selectedShapes.length !== 1 || !['document', 'actor'].includes(selectedShape.type)) {
+        return
+    }
+    schema.unshift({
+        id: 'foundry-document-group',
+        type: 'group',
+        checkbox: false,
+        disabled: false,
+        readonlyOk: true,
+        children: [
+            {
+                id: 'render-sheet',
+                type: 'item',
+                actionItem: {
+                    id: 'action.toggle-grid',
+                    label: game.i18n.localize('JW.OpenSheet'),
+                    readonlyOk: true,
+                    onSelect: async () => {
+                        const document = await fromUuid(selectedShape.props.id)
+                        document.sheet.render(true)
+                    },
+                },
+                checked: true,
+                readonlyOk: true,
+                disabled: !selectedShape?.props?.id
+            },
+        ],
+    });
+}
