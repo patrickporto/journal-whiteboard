6744bd510f6c0ec85b955a8008174406244f108c
Patrick Porto <patrick.s.porto@gmail.com>
2023-06-05 15:18:28 -0600

    chore: fix hot reload

---
+++ src/journal-whiteboard.js
@@ -0,0 +1,11 @@
+import RefreshRuntime from "/@react-refresh";
+RefreshRuntime.injectIntoGlobalHook(window);
+window.$RefreshReg$ = () => {};
+window.$RefreshSig$ = () => (type) => type;
+window.__vite_plugin_react_preamble_installed__ = true;
+
+window.global = window;
+
+import "/@vite/client"
+
+import("./main.ts");
