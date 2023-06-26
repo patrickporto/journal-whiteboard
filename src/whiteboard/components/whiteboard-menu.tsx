21c598f1ad5befd6bf83eba2eddbc876f1125a3c
Patrick Porto <patrick.porto@loft.com.br>
2023-06-25 23:46:06 -0600

    feat: add align buttons

--- src/whiteboard/components/whiteboard-menu.tsx
+++ src/whiteboard/components/whiteboard-menu.tsx
@@ -27,8 +27,121 @@ export const WhiteboardMenu = () => {
         app.duplicateShapes(ids, offset);
     }, [app]);
 
+    const handleAlignLeft = useCallback(() => {
+        if (!app || app.currentToolId !== 'select') return;
+        app.mark('align left')
+        app.alignShapes('left', app.selectedIds)
+    }, [app])
+    const handleAlignHorizontally = useCallback(() => {
+        if (!app || app.currentToolId !== 'select') return;
+        app.mark('align center horizontal')
+        app.alignShapes('center-horizontal', app.selectedIds)
+    }, [app])
+    const handleAlignRight = useCallback(() => {
+        if (!app || app.currentToolId !== 'select') return;
+        app.mark('align right')
+        app.alignShapes('right', app.selectedIds)
+    }, [app])
+    const handleAlignTop = useCallback(() => {
+        if (!app || app.currentToolId !== 'select') return;
+        app.mark('align top')
+        app.alignShapes('top', app.selectedIds)
+    }, [app])
+    const handleAlignVertically = useCallback(() => {
+        if (!app || app.currentToolId !== 'select') return;
+        app.mark('align center vertical')
+        app.alignShapes('center-vertical', app.selectedIds)
+    }, [app])
+    const handleAlignBottom = useCallback(() => {
+        if (!app || app.currentToolId !== 'select') return;
+        app.mark('align bottom')
+        app.alignShapes('bottom', app.selectedIds)
+    }, [app])
+    const handleDistributeHorizontally = useCallback(() => {
+        if (!app || app.currentToolId !== 'select') return;
+        app.mark('distribute horizontal')
+        app.distributeShapes('horizontal', app.selectedIds)
+    }, [app])
+    const handleDistributeVertically = useCallback(() => {
+        if (!app || app.currentToolId !== 'select') return;
+        app.mark('distribute vertical')
+        app.distributeShapes('vertical', app.selectedIds)
+    }, [app])
+
     return (
         <Menu>
+        <li>
+            <MenuButton
+                type="button"
+                data-tooltip={game.i18n.localize('JW.AlignLeft')}
+                onClick={handleAlignLeft}
+            >
+                <i className="fa-solid fa-objects-align-left"></i>
+            </MenuButton>
+        </li>
+        <li>
+            <MenuButton
+                type="button"
+                data-tooltip={game.i18n.localize('JW.AlignHorizontally')}
+                onClick={handleAlignHorizontally}
+            >
+                <i className="fa-solid fa-objects-align-center-horizontal"></i>
+            </MenuButton>
+        </li>
+        <li>
+            <MenuButton
+                type="button"
+                data-tooltip={game.i18n.localize('JW.AlignRight')}
+                onClick={handleAlignRight}
+            >
+                <i className="fa-solid fa-objects-align-right"></i>
+            </MenuButton>
+        </li>
+        <li>
+            <MenuButton
+                type="button"
+                data-tooltip={game.i18n.localize('JW.AlignTop')}
+                onClick={handleAlignTop}
+            >
+                <i className="fa-solid fa-objects-align-top"></i>
+            </MenuButton>
+        </li>
+        <li>
+            <MenuButton
+                type="button"
+                data-tooltip={game.i18n.localize('JW.AlignVertically')}
+                onClick={handleAlignVertically}
+            >
+                <i className="fa-solid fa-objects-align-center-vertical"></i>
+            </MenuButton>
+        </li>
+        <li>
+            <MenuButton
+                type="button"
+                data-tooltip={game.i18n.localize('JW.AlignBottom')}
+                onClick={handleAlignBottom}
+            >
+                <i className="fa-solid fa-objects-align-bottom"></i>
+            </MenuButton>
+        </li>
+        <li>
+            <MenuButton
+                type="button"
+                data-tooltip={game.i18n.localize('JW.DistributeHorizontally')}
+                onClick={handleDistributeHorizontally}
+            >
+            <i className="fa-solid fa-distribute-spacing-horizontal"></i>
+            </MenuButton>
+        </li>
+        <li>
+            <MenuButton
+                type="button"
+                data-tooltip={game.i18n.localize('JW.DistributeVertically')}
+                onClick={handleDistributeVertically}
+            >
+                <i className="fa-solid fa-distribute-spacing-vertical"></i>
+            </MenuButton>
+        </li>
             <li>
                 <MenuButton
                     type="button"
